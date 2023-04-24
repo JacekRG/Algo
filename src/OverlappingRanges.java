c5d7207a5351904ac52068ea1d10b1145144dce7
Jacek <steiner@10g.pl>
2023-04-24 19:19:47 +0200

    Task 33

---
+++ src/OverlappingRanges.java
@@ -0,0 +1,30 @@
+public class OverlappingRanges {
+
+    public static boolean OverlappingRanges(int[] arr) {
+
+        int lapCounter = 0;
+        boolean isTrue = false;
+
+        if(arr[2] >= arr[0] &&  arr[3] <= arr[1]){
+            lapCounter = arr[3] - arr[2] + 1;
+            isTrue = lapCounter == arr[arr.length - 1];
+        }
+
+        if(arr[2] >= arr[0] &&  arr[3] > arr[1]){
+            lapCounter = arr[1] - arr[2]  +1;
+            isTrue = lapCounter == arr[arr.length - 1];
+        }
+
+        if(arr[2] < arr[0] &&  arr[3] <= arr[1]){
+            lapCounter = arr[3] - arr[0] + 1;
+            isTrue = lapCounter == arr[arr.length - 1];
+        }
+
+        if(arr[2] < arr[0] &&  arr[3] > arr[1]){
+            lapCounter = arr[1] - arr[0] + 1;
+            isTrue = lapCounter == arr[arr.length - 1];
+
+        }
+        return isTrue;
+    }
+}
