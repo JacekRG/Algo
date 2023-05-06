226ae8c14886a0f87e3dea1ef8041759c31653a2
Jacek <steiner@10g.pl>
2023-05-06 16:09:25 +0200

    Stack added

---
+++ src/Somelist.java
@@ -0,0 +1,17 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Somelist {
+
+    void boringmethod(){
+
+        LinkedList<String> linkedList = new LinkedList<>();
+        Queue<Integer> integers = new PriorityQueue<>(Collections.reverseOrder() );
+        Stack<String> stack = new Stack<>();
+
+    }
+
+}
